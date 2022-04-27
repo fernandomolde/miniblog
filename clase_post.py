9277241dd64b5bbb557c4733d287b15955a03e14
fernandomolde <ortizmoldef@gmail.com>
2022-04-27 10:40:37 +0200

    nuevo_codigo

---
+++ clase_post.py
@@ -0,0 +1,7 @@
+class Posts():
+    def __init__(self, id=None, fecha=None, autor=None, titulo=None, cuerpo=None) -> None:
+        self.id = id
+        self.fecha = fecha
+        self.autor = autor
+        self.titulo = titulo
+        self.cuerpo = cuerpo
